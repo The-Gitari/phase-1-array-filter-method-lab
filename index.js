ab68b2435a7d14dcfa13c7c47bbfb9a4843084b8
Noel Gitari <gitarinoel@gmail.com>
2023-03-23 23:06:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter (driver =>
+      driver.toLowerCase() === string.toLowerCase()
+    );
+  }
+
+function fuzzyMatch(drivers,string){
+    return drivers.filter (driver =>driver.slice(0,1)===string.slice(0,1));
+
+}
+function matchName(driver,string){
+
+    return driver.filter(driver=>driver.name.toLowerCase()===string.toLowerCase())
+
+}
